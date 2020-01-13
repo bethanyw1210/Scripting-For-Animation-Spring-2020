82ffd4ab4269a38f57a33719489b5dbebc13a4c0
Bethany Woodruff <bethany.w1210@gmail.com>
2020-01-13 11:14:23 -0700

    writing our first script, learned start and update differences

---
+++ Class Work Spring 2020/Assets/Scripts/NewClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hi");
+    }
+}
