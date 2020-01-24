1bc30505c748a12643d8291ed89acfe40f229115
Bethany Woodruff <bethany.w1210@gmail.com>
2020-01-23 20:30:26 -0700

    created movement script for game

--- Final Game/Assets/Scripts/PlayerMovement.cs
+++ Final Game/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f, jumpHeight = 10f;
-    public float gravity = -9.8f;
-    
+    private Vector3 position;
+    private CharacterController controller;
+
+    public float speed = 10f, gravity = -2f, jumpSpeed = 32f;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
-
     
     void Update()
     {
-        
+        position.x = speed * Input.GetAxis("Horizontal");
+
+        if (controller.isGrounded)
+        {
+            position.y = 0f;
+        }
+
+        if (!controller.isGrounded)
+        {
+            position.y += gravity;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        controller.Move (position * Time.deltaTime);
     }
 }
