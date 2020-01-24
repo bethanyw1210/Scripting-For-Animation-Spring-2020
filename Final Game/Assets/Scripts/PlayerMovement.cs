aea0d4c05bc2ff830388dbc10fb8328fc6f23f03
Bethany Woodruff <bethany.w1210@gmail.com>
2020-01-23 19:49:29 -0700

    added sketchbook pages

---
+++ Final Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f, jumpHeight = 10f;
+    public float gravity = -9.8f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
