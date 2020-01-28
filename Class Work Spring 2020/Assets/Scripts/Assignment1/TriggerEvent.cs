a8667c903b2f089f59790a8bfbbccc2a2b6c44d5
Bethany Woodruff <bethany.w1210@gmail.com>
2020-01-28 09:16:43 -0700

    added ontriggerexit and stay events for homework

--- Class Work Spring 2020/Assets/Scripts/Assignment1/TriggerEvent.cs
+++ Class Work Spring 2020/Assets/Scripts/Assignment1/TriggerEvent.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvent : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent, triggerExitEvent, triggerStayEvent;
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStayEvent.Invoke();
     }
 }
