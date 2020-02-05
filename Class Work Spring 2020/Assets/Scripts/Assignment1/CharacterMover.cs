f0cd6b40aada18b57fdafc001b15a4c286e22bc5
Bethany Woodruff <bethany.w1210@gmail.com>
2020-02-05 11:16:38 -0700

    created a character controller in class

---
+++ Class Work Spring 2020/Assets/Scripts/Assignment1/CharacterMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 10f, gravity = -3f, jumpForce = 30f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y -= gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
