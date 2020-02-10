5643772d984b880bff5f0d86745630a92a211cb4
Bethany Woodruff <bethany.w1210@gmail.com>
2020-02-10 11:13:46 -0700

    double jump, rigidbodies, physics, if statements in class

--- Class Work Spring 2020/Assets/Scripts/Assignment1/CharacterMover.cs
+++ Class Work Spring 2020/Assets/Scripts/Assignment1/CharacterMover.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class CharacterMover : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     public Vector3 positionDirection;
-    public float speed = 10f, gravity = -3f, jumpForce = 30f;
+    public float speed = 10f, gravity = 3f, jumpForce = 30f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
+        
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
         
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
         
         positionDirection.y -= gravity;
---
+++ Class Work Spring 2020/Assets/Scripts/Assignment2/IfStatements.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+    public int a = 10;
+    public int b = 4;
+    public int c = 14;
+    
+    public string password = "1234";
+    
+    public bool canRun;
+    
+    void Start()
+    {
+        if (a + b != c)
+        {
+            print ("this is true");
+        }
+
+        if (password != "1234")
+        {
+            print("correct password");
+        }
+
+        if (!canRun)
+        {
+            print("we can run");
+        }
+    }
+}
