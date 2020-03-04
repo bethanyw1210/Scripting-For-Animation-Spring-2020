35a60c3d4efa01ca094dda362f9643d81d252a3c
Bethany Woodruff <bethany.w1210@gmail.com>
2020-03-04 11:15:04 -0700

    vector 3 data and checkpoints

---
+++ Class Work Spring 2020/Assets/Scripts/Assignment2/MonoBehaviors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoBehaviors : MonoBehaviour
+{
+    public UnityEvent startEvent;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+}
---
+++ Class Work Spring 2020/Assets/Scripts/Assignment2/StringData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StringData : ScriptableObject
+{
+    public string stringName;
+
+    public void StringName()
+    {
+        stringName = name;
+    }
+}
