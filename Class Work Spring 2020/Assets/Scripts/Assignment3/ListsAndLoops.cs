5eccf5c867c3f0bd1ce719838e4df22659c5562a
Bethany Woodruff <bethany.w1210@gmail.com>
2020-03-11 11:14:58 -0600

    lists and loops

---
+++ Class Work Spring 2020/Assets/Scripts/Assignment3/ListsAndLoops.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ListsAndLoops : ScriptableObject
+{
+    //array size cannot be changed via code
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+    
+    public void CheckList()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj == singleString)
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+}
