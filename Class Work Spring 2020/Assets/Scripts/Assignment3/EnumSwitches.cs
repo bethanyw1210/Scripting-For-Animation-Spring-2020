c586868bd5caf58e7d7a24072ce77f26ad04d057
Bethany Woodruff <bethany.w1210@gmail.com>
2020-03-23 11:16:37 -0600

    enums and switch statements class lesson

---
+++ Class Work Spring 2020/Assets/Scripts/Assignment3/EnumSwitches.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumSwitches : MonoBehaviour
+{
+    public UnityEvent idleEvent, walkEvent, runEvent, jumpEvent;
+    public UnityEvent startingEvent, playingEvent, inStoreEvent, pausingEvent, endingEvent;
+    
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        InStore,
+        Pausing,
+        Ending
+    }
+
+    public GameStates currentGameState;
+
+    public enum PlayerStates
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump
+    }
+
+    public PlayerStates currentPlayerState;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentPlayerState)
+        {
+            case PlayerStates.Idle:
+                idleEvent.Invoke();
+                break;
+            case PlayerStates.Walk:
+                walkEvent.Invoke();
+                break;
+            case PlayerStates.Run:
+                runEvent.Invoke();
+                break;
+            case PlayerStates.Jump:
+                jumpEvent.Invoke();
+                break;
+            default:
+                print("I'm anything");
+                break;
+        }
+
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                startingEvent.Invoke();
+                break;
+            case GameStates.Playing:
+                playingEvent.Invoke();
+                break;
+            case GameStates.InStore:
+                inStoreEvent.Invoke();
+                break;
+            case GameStates.Pausing:
+                pausingEvent.Invoke();
+                break;
+            case GameStates.Ending:
+                endingEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
