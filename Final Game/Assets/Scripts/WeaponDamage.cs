eb7ce6af00463eb78083453d9b06a7c3574ad22f
Bethany Woodruff <bethany.w1210@gmail.com>
2020-04-04 22:39:28 -0600

    Started on weapon damage script

---
+++ Final Game/Assets/Scripts/WeaponDamage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    public string objTag;
+    public GameObject enemy;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == objTag)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
