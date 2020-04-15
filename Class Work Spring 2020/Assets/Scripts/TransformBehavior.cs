3e082408757322de2e621c1cc5477e347c0b673d
Bethany Woodruff <bethany.w1210@gmail.com>
2020-04-15 11:21:13 -0600

    platform movement in class

---
+++ Class Work Spring 2020/Assets/Scripts/TransformBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformBehavior : MonoBehaviour
+{
+    public string otherTagString;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(otherTagString))
+        {
+            transform.parent = other.transform;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
