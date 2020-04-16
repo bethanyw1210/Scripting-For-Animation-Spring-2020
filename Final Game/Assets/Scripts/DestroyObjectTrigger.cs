8d57be1db2ea602be1b4e9941a8a49f49386d337
Bethany Woodruff <bethany.w1210@gmail.com>
2020-04-15 20:26:39 -0600

    Added triggers and art to health potions

--- Final Game/Assets/Scripts/DestroyObjectTrigger.cs
+++ Final Game/Assets/Scripts/DestroyObjectTrigger.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DestroyObjectTrigger : MonoBehaviour
 {
     public string objTag;
+    public UnityEvent particleSystem;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == objTag)
         {
+            particleSystem.Invoke();
             Destroy(gameObject);
         }
     }
