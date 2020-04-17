18fa358ceebf1e5269625456cec48171f1b1353d
Bethany Woodruff <bethany.w1210@gmail.com>
2020-04-16 20:00:47 -0600

    Enemy list

---
+++ Final Game/Assets/Scripts/RemovePotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePotion : MonoBehaviour
+{
+    public List<GameObject> potionObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < potionObj.Count; i++)
+        {
+            potionObj.Remove(other.gameObject);
+        }
+    }
+
+    public void NameList()
+    {
+        foreach (var obj in potionObj)
+        {
+            Debug.Log(obj);
+        }
+    }
+}
